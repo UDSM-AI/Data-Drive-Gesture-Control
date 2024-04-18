79335ccc86a7b0442a8aa0eb2d956e70b11810c1
eddygulled <edgargulay69@gmail.com>
2024-04-19 01:29:18 +0300

    pro-code decoupling

--- src/steer_direction.py
+++ src/steer_direction.py
@@ -4,7 +4,17 @@ import numpy as np
 from src.drive_actions import perform_action, release_accelerate_keys, release_steering_keys, release_forward_keys, release_reverse_keys, release_left_keys, release_right_keys
 from src.finger_actions import *
 
-def track_hand():
+
+def track_hand() -> None:
+    """
+    Tracks hand gestures using MediaPipe Hand tracking and controls actions accordingly.
+
+    This function initializes hand tracking using the MediaPipe library, captures video frames from the webcam, 
+    and detects hand landmarks. Based on the detected hand gestures, it performs corresponding actions.
+
+    Returns:
+        None
+    """
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands()
     mp_drawing = mp.solutions.drawing_utils
@@ -30,23 +40,31 @@ def track_hand():
             mask = np.zeros_like(frame)
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(mask, hand_landmarks, mp_hands.HAND_CONNECTIONS,
-                                        mp_drawing.DrawingSpec(color=(255, 255, 255), thickness=2,
-                                                                circle_radius=2),
-                                        mp_drawing.DrawingSpec(color=(255, 255, 255), thickness=2,
-                                                                circle_radius=2))
+                                           mp_drawing.DrawingSpec(color=(255, 255, 255), thickness=2,
+                                                                  circle_radius=2),
+                                           mp_drawing.DrawingSpec(color=(255, 255, 255), thickness=2,
+                                                                  circle_radius=2))
 
                 detect_hand_action(hand_landmarks, frame, width, height)
 
             frame = cv2.bitwise_and(frame, mask)
 
-        cv2.rectangle(frame, (left_hand_region[0], left_hand_region[1]),
-                    (left_hand_region[0] + left_hand_region[2], left_hand_region[1] + left_hand_region[3]),
-                    (255, 0, 0), 2)
-        cv2.rectangle(frame, (right_hand_region[0], right_hand_region[1]),
-                    (right_hand_region[0] + right_hand_region[2], right_hand_region[1] + right_hand_region[3]),
-                    (255, 0, 0), 2)
+            cv2.rectangle(frame, (left_hand_region[0], left_hand_region[1]),
+                        (left_hand_region[0] + left_hand_region[2], left_hand_region[1] + left_hand_region[3]),
+                        (255, 255, 20), 2)
+            cv2.rectangle(frame, (right_hand_region[0], right_hand_region[1]),
+                        (right_hand_region[0] + right_hand_region[2], right_hand_region[1] + right_hand_region[3]),
+                        (255, 255, 20), 2)
+        else:
+            cv2.rectangle(frame, (left_hand_region[0], left_hand_region[1]),
+                        (left_hand_region[0] + left_hand_region[2], left_hand_region[1] + left_hand_region[3]),
+                        (255, 155, 20), 2)
+            cv2.rectangle(frame, (right_hand_region[0], right_hand_region[1]),
+                        (right_hand_region[0] + right_hand_region[2], right_hand_region[1] + right_hand_region[3]),
+                        (255, 155, 20), 2)
 
-        cv2.imshow('Data Drive', frame)
+
+        cv2.imshow('Data Drive : Gesture Control', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -54,7 +72,20 @@ def track_hand():
     cap.release()
     cv2.destroyAllWindows()
 
-def detect_hand_action(hand_landmarks, frame, width, height):
+
+def detect_hand_action(hand_landmarks: Any, frame: np.ndarray, width: int, height: int) -> None:
+    """
+    Detects hand gestures and performs corresponding actions.
+
+    Args:
+        hand_landmarks: Detected hand landmarks by MediaPipe.
+        frame: Video frame.
+        width: Width of the video frame.
+        height: Height of the video frame.
+
+    Returns:
+        None
+    """
     palm_landmark = hand_landmarks.landmark[mp.solutions.hands.HandLandmark.WRIST]
     palm_x = int(palm_landmark.x * width)
     palm_y = int(palm_landmark.y * height)
@@ -64,7 +95,18 @@ def detect_hand_action(hand_landmarks, frame, width, height):
     else:
         detect_right_hand_action(hand_landmarks, frame)
 
-def detect_left_hand_action(hand_landmarks, frame):
+
+def detect_left_hand_action(hand_landmarks: Any, frame: np.ndarray) -> None:
+    """
+    Detects gestures of the left hand and performs corresponding actions.
+
+    Args:
+        hand_landmarks: Detected hand landmarks by MediaPipe.
+        frame: Video frame.
+
+    Returns:
+        None
+    """
     if is_index_finger_touching_thumb(hand_landmarks):
         cv2.putText(frame, "Accelerate", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         print("Accelerate")
@@ -84,7 +126,18 @@ def detect_left_hand_action(hand_landmarks, frame):
         cv2.putText(frame, "No Action Detected", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         release_accelerate_keys()
 
-def detect_right_hand_action(hand_landmarks, frame):
+
+def detect_right_hand_action(hand_landmarks: Any, frame: np.ndarray) -> None:
+    """
+    Detects gestures of the right hand and performs corresponding actions.
+
+    Args:
+        hand_landmarks: Detected hand landmarks by MediaPipe.
+        frame: Video frame.
+
+    Returns:
+        None
+    """
     if is_index_finger_touching_thumb(hand_landmarks):
         cv2.putText(frame, "Steering Left", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         print("Steering Left")
